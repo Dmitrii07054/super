23103a9969abc9f78576e37ee71b790cfa64ba18
Dmitrii07054 <dmikhailov@gmail.com>
2025-10-15 18:33:51 +0300

    Рефакторинг кода 15.10.25

--- 251015_for_refactoring/after_refactoring.cpp
+++ 251015_for_refactoring/after_refactoring.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main() {
-	const int size = 3;	
+	const int size = 10;	
     int arr[size];
     for (int i = 0; i < size; i++) {
         std::cout << "Введите " << i << " элемент" << std::endl;
