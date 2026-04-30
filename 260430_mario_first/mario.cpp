69007a568da8097cccffd258f0b0fcc066d06d55
Dmitrii07054 <dmikhailov@gmail.com>
2026-05-01 00:58:24 +0300

    сделал персонажа

--- 260430_mario_first/mario.cpp
+++ 260430_mario_first/mario.cpp
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define mapWidth 80
 #define mapHeight 25
 
+typedef struct SObject {
+	float x,y;
+	float width, height;
+} TObject;
+
+
 char map[mapHeight][mapWidth+1];
+TObject mario;
+
 
 void ClearMap()
 {
@@ -19,13 +28,45 @@ void ShowMap()
 {
 	map[mapHeight - 1][mapWidth - 1] = '\0';
 	for (int j = 0; j < mapHeight; j++)
-		printf("%s", map[j]);
+		printf("%80s", map[j]);
 }
+
+void SetObjectPos(TObject *obj, float xPos, float yPos)
+{
+	(*obj) .x = xPos;
+	(*obj) .y = yPos;
+}
+
+void InitObject(TObject *obj, float xPos, float yPos, float oWidth, float oHeight)
+{
+	SetObjectPos(obj, xPos, yPos);
+	(*obj).width = oWidth;
+	(*obj).height = oHeight;
+}
+
+void PutObjectOnMap(TObject obj)
+{
+	int ix = (int)round(obj.x);
+	int iy = (int)round(obj.y);
+	int iWidth = (int)round(obj.width);
+	int iHeight = (int)round(obj.height);
+	
+	for (int i = ix; i < (ix + iWidth); i++)
+		for (int j = iy; j < (iy + iHeight); j++)
+			map[j][i] = '@';
+}
+
 
 int main()
 {
+	InitObject(&mario, 38, 10, 3, 3);
 	ClearMap();
-	ShowMap();
+	PutObjectOnMap(mario);
 	
+	
+	ShowMap();
 	return 0;
 }
+
+
+
