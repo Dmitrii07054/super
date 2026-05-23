ee15ab9add3ffc3728feb1727846baed48a56fb9
Dmitrii07054 <dmikhailov@gmail.com>
2026-05-23 10:26:48 +0300

    mario_oop_1

--- 260522_mario_with_OOP/Brick.cpp
+++
@@ -1,8 +0,0 @@
-#include "Brick.hpp"
-
-Brick::Brick() : GameObject() {}
-
-Brick::Brick(float xPos, float yPos, float w, float h, ObjectType t)
-    : GameObject(xPos, yPos, w, h, t) {}
-
-void Brick::update() {}
--- 260522_mario_with_OOP/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-g++ GameObject.cpp Brick.cpp MovingObject.cpp Game.cpp main.cpp -o Mario.exe
-Mario.exe
-pause
